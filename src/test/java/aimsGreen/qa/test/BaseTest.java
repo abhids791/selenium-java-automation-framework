9cad0dc764ea202d9df8102ad2127be998e5e63b
Abhids788 <abhids788@gmail.com>
2026-09-08 16:38:17 +0530

    implemented e2e

--- src/test/java/aimsGreen/qa/test/BaseTest.java
+++ src/test/java/aimsGreen/qa/test/BaseTest.java
@@ -1,4 +1,50 @@
 package aimsGreen.qa.test;
 
+import AimsGreen.QA.pages.LandingPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 public class BaseTest {
+    WebDriver driver;
+    LandingPage landingPage;
+
+    @BeforeMethod
+    public LandingPage launchApplication() throws IOException {
+        driver=initializeDriver();
+        landingPage= new LandingPage(driver);
+        landingPage.goToLandingPage();
+        return landingPage;
+    }
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    public WebDriver initializeDriver() throws IOException {
+        Properties prop= new Properties();
+        FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
+        prop.load(fis);
+        String browserName= prop.getProperty("browser");
+
+        if(browserName.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("FireFox")) {
+            driver = new FirefoxDriver();
+        }else if (browserName.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+    }
 }
