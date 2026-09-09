6219859fc6579bc1f71843d8640f272ba917f8e2
Abhids788 <abhids788@gmail.com>
2026-09-09 23:15:49 +0530

    updated for wrong message

--- src/test/java/brightlyTestQa/test/ErrorValidationTest.java
+++ src/test/java/brightlyTestQa/test/ErrorValidationTest.java
@@ -28,7 +28,7 @@ public class ErrorValidationTest extends BaseTest {
         landingPage.enterValueInPassengersInputField("2");
         landingPage.clickOnOneWay();
         landingPage.clickOnSearchFlightButton();
-        Assert.assertEquals(landingPage.getFromFieldErrorMessage(), "Please select a departure city.");
+        Assert.assertEquals(landingPage.getFromFieldErrorMessage(), "Please select a departure city");
     }
 
     @Test(groups = {"Regression", "Smoke"}, retryAnalyzer = RetryWhenTestFailed.class)
@@ -40,6 +40,6 @@ public class ErrorValidationTest extends BaseTest {
         landingPage.enterValueInPassengersInputField("2");
         landingPage.clickOnOneWay();
         landingPage.clickOnSearchFlightButton();
-        Assert.assertEquals(landingPage.getDepartureDateFieldErrorMessage(), "Please select a departure date.");
+        Assert.assertEquals(landingPage.getDepartureDateFieldErrorMessage(), "Please select a departure date");
     }
 }
