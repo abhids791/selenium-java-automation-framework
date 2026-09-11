9aa57c594c50a948f491b21e6666ece409900fd5
Abhids788 <abhids788@gmail.com>
2026-09-12 01:37:06 +0530

    updated Source again

--- src/test/java/brightlyTestQa/test/BookFlightTwoWay.java
+++ src/test/java/brightlyTestQa/test/BookFlightTwoWay.java
@@ -16,7 +16,7 @@ public class BookFlightTwoWay extends BaseTest {
     public void bookFlightTwoWay() throws InterruptedException, IOException {
 
         //Enter Search Details
-        landingPage.selectValueInFromDropDown("Sydney");
+        landingPage.selectValueInFromDropDown("Paris");
         landingPage.selectValueInToDropDown("Delhi");
         landingPage.selectValueFromDepartureDate("09-09-2026");
         landingPage.selectValueFromReturnDate("16-09-2026");
