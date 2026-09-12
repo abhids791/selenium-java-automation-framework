4d7120de94e332285bca6b478338dd373d77738b
Abhids788 <abhids788@gmail.com>
2026-09-12 13:07:10 +0530

    updated failed regression

--- src/test/java/brightlyTestQa/test/ErrorValidationTest.java
+++ src/test/java/brightlyTestQa/test/ErrorValidationTest.java
@@ -17,7 +17,7 @@ public class ErrorValidationTest extends BaseTest {
         landingPage.enterValueInPassengersInputField("2");
         landingPage.clickOnOneWay();
         landingPage.clickOnSearchFlightButton();
-        Assert.assertEquals(landingPage.getToFieldErrorMessage(), "Please select a destination city.");
+        Assert.assertEquals(landingPage.getToFieldErrorMessage(), "Please select a destination city. Failed for Regression");
     }
 
     @Test(groups = {"Regression", "Smoke"}, retryAnalyzer = RetryWhenTestFailed.class)
